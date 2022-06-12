3d7cbbc9e2140f7f045404df9195b73a36338398
Whitney Bluzma <whitney@accommodationforstudents.com>
2022-06-12 23:37:32 +0100

    feat: config returns node environment

--- __tests__/unit/config/config.test.ts
+++ __tests__/unit/config/config.test.ts
@@ -32,4 +32,46 @@ describe("getConfig", () => {
       expect(config.port).toBe(5000);
     });
   });
+
+  describe("NODE_ENV", () => {
+    it("returns development env from process.env if NODE_ENV is set to development", () => {
+      process.env = {
+        NODE_ENV: "development",
+      };
+
+      const config: Config = getConfig();
+
+      expect(config.env).toBe("development");
+    });
+
+    it("returns test env from process.env if NODE_ENV is set to test", () => {
+      process.env = {
+        NODE_ENV: "test",
+      };
+
+      const config: Config = getConfig();
+
+      expect(config.env).toBe("test");
+    });
+
+    it("returns production env from process.env if NODE_ENV is undefined", () => {
+      process.env = {
+        NODE_ENV: undefined,
+      };
+
+      const config: Config = getConfig();
+
+      expect(config.env).toBe("production");
+    });
+
+    it("returns production env from process.env if NODE_ENV is not a known environment", () => {
+      process.env = {
+        NODE_ENV: "holiday",
+      };
+
+      const config: Config = getConfig();
+
+      expect(config.env).toBe("production");
+    });
+  });
 });
--- src/config/index.ts
+++ src/config/index.ts
@@ -1,18 +1,29 @@
+const environments = ["development", "test", "production"] as const;
+type Environment = typeof environments[number];
+
 export interface Config {
   port: number;
-  // we will add the environment property later
+  env: Environment;
 }
 
-export default (): Config => {
-  const port = process.env.PORT;
-  const defaultPort = 5000;
+const isEnvironment = (env: string | undefined): env is Environment =>
+  environments.includes(env as Environment);
 
-  if (typeof port !== "string" || isNaN(Number(port)))
-    return {
-      port: defaultPort,
-    };
+export default (): Config => {
+  const defaultPort = 5000;
+  const defaultEnv = "production";
+
+  const port: number =
+    typeof process.env.PORT === "string" && !isNaN(Number(process.env.PORT))
+      ? Number(process.env.PORT)
+      : defaultPort;
+
+  const env: Environment = isEnvironment(process.env.NODE_ENV)
+    ? process.env.NODE_ENV
+    : defaultEnv;
 
   return {
-    port: Number(port),
+    port,
+    env,
   };
 };
