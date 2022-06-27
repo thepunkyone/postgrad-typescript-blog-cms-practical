5865ee5051450abe0d293b1027386bf5db525ae8
Whitney Bluzma <thepunkyone@gmail.com>
2022-06-27 02:44:51 +0100

    fix: try again with js

--- dist/src/index.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const app_1 = require("./app");
-const config_1 = require("./config");
-const conf = (0, config_1.default)();
-app_1.default.listen(conf.port, () => {
-    console.log(`app is listening on port ${conf.port}`);
-});
-//# sourceMappingURL=index.js.map
